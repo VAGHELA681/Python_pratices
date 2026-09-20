6c37776d2db3393728290632c40f2f9644edcd2d
Vaghela Madhuri <madhurivaghela463@gmail.com>
2026-09-20 15:05:34 +0530

    list.py
    [1, 2, 3, 4, 5, 6, 7, 8, 9]
    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
    ['madhuri', 30, 'una', 900]
    ['madhvi', 30, 'una', 900]
    
    List એટલે એક જ જગ્યાએ ઘણી values store કરવા માટેનું collection.
    
    List ને આપણે square brackets [ ] માં લખીએ છીએ.
    
    [ ] → List માટે
    
    Multiple values રાખી શકાય
    
    Duplicate values રાખી શકાય
    
    List ને change કરી શકાય (mutable)
    
    A list is a collection of multiple values stored in a single variable.

---
+++ list.py
@@ -0,0 +1,10 @@
+num = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9]
+print(num)
+num.append(10)
+print(num)
+
+student = ["madhuri" , 30 , "una" , 900]
+print(student)
+
+student[0] = "madhvi"
+print(student)
